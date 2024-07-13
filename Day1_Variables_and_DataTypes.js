c8defcac3d9477d9c5378b707be8143ac0a7f926
MD SIDDIQUE <76256885+Md-Siddique-Git@users.noreply.github.com>
2024-07-13 14:31:05 +0530

    Add files via upload
    learn About Variables and data types

---
+++ Day1_Variables_and_DataTypes.js
@@ -0,0 +1,41 @@
+// Activity 1 
+// Task1:Variable Declaration
+var age=24
+let name='Siddique'
+// console.log(name ,age);
+// Task2:constant Declaration
+const adult=true
+// console.log(adult);
+
+// Activity 3: Data Types
+var number =23
+let string ='Eneter'
+let boolean=true
+
+const person={
+    name:'siddique',
+    age:24
+
+
+}
+const fruits=['apple','cherry','mango']
+
+// console.log(typeof(number));
+// console.log(typeof(string));
+
+// console.log(typeof(boolean));
+
+// console.log(typeof(person));
+// console.log(Array.isArray(fruits));
+
+// Activity 4.............///////////// Reassigning
+let number1=34
+console.log(number1);
+
+number1=23
+console.log(number1);
+
+// Activity 5 reassing const
+const ram='rahul'
+ram='ranjan'
+console.log(ram);
